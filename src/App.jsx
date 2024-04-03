d1c83def44040a208cf408e21a7462d6f4311874
mgutbor <mgutbor@gmail.com>
2024-04-03 19:13:37 +0200

    añadiendo funcionalidad de borrado de lista

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ import {useColors} from "ColorPicker/useColors";
 import "./index.css";
 
 const App = () => {
-  const { color, colorsList, handleChangeColor, handleSubmitSaveColor } = useColors();
+  const { color, colorsList, handleChangeColor, handleSubmitSaveColor, handleClickClearColors } = useColors();
   return (
   <>
     <h1 className="text-center bg-dark text-white p-4">Color Picker</h1>
     <div className="container mt-4">
       <div className="row">
-        <div className="col-12 col-md-4"><ColorList colorsList={colorsList} /></div>
+        <div className="col-12 col-md-4"><ColorList colorsList={colorsList} handleClickClearColors={handleClickClearColors} /></div>
         <div className="col-12 col-md-8"><ColorPicker color={color} handleChangeColor={handleChangeColor} handleSubmitSaveColor={handleSubmitSaveColor} /></div>
       </div>
     </div>
